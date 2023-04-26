a2d684600079db2e52d62453aac07bf5f93f54e4
justkaram <karamhilles@gmail.com>
2023-04-26 09:26:43 +0200

    Adding comments in search_image

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
@@ -15,18 +17,29 @@ Builder.load_file('frontend.kv')
 class FirstScreen(Screen):
 
     def search_image(self):
-        self.manager.current_screen.ids.img.source = 'files/laptop.jpg'
-        text = self.manager.current_screen.ids.txt_input.text
+
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.txt_input.text
+
+        # Get an image matching the query using wikipedia lib
         headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58',
         }
         try:
-            page = wikipedia.page(text)
+            # Get the image link
+            page = wikipedia.page(query)
             link = page.images[0]
             r = requests.get(link, headers=headers)
-            with open(f'{text}.jpg', 'wb') as f:
+
+            # Save the image in the same directory
+            with open(f'files/{query}.jpg', 'wb') as f:
                 f.write(r.content)
-        except Exception:
+
+            time.sleep(2)
+            # Set the FirstScreen image to the downloaded image
+            self.manager.current_screen.ids.img.source = f'files/{query}.jpg'
+        except wikipedia.exceptions.DisambiguationError:
             return 'Try being more specific'
 
 
