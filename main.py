56edeb43536361cbe039c04865a7220b0320d8e3
justkaram <karamhilles@gmail.com>
2023-04-26 08:23:24 +0200

    Changing the size of the widgets

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+
 """
 Kivy app is made from 4 objects: App, ScreenManager, Screen, Widget.
 This code is called boilerplate. We will use these 4 classes with every Kivy app.
 """
 
 Builder.load_file('frontend.kv')
+
+
 class FirstScreen(Screen):
 
     def search_image(self):
